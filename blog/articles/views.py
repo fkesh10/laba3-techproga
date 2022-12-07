03961c819187953845b74405cca6ffd48f386303
DrukardKirA <fkesh10@gmail.com>
2022-12-07 17:56:47 +0300

    work is done!

--- blog/articles/views.py
+++ blog/articles/views.py
@@ -1,7 +1,9 @@
-from models import Article
+from articles.models import Article
 from django.shortcuts import render
 
 def archive(request):
     return render(request, 'archive.html', {"posts": Article.objects.all()})
 
+# def archive(request):
+#     return render(request, 'templates/archive.html')
 # Create your views here.
